c6f1ccc316eb67906017c7241218cb328dcf8b0c
Emmanuel <emmsom506@gmail.com>
2025-01-05 22:17:26 +0000

    add profile menu list paper

---
+++ src/components/shared/ui/menu-paper.tsx
@@ -0,0 +1,140 @@
+'use client';
+
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import React from "react";
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+
+interface ProfileMenuListInterface {
+  key: string,
+  label: string,
+  icon: React.ReactNode,
+  onClick: () => void
+}
+const ProfileMenuPaper = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const profileMenu: ProfileMenuListInterface [] = [
+    {
+      key: "myRecipes",
+      label: "My Recipes",
+      icon: <MenuBookIcon/>,
+      onClick: handleClose
+    },
+    {
+      key: "accountSettings",
+      label: "Account Settings",
+      icon: <Settings/>,
+      onClick: handleClose
+    },
+    {
+      key: "notifications",
+      label: "Notifications",
+      icon: <NotificationsIcon/>,
+      onClick: handleClose
+    },
+    {
+      key: "helpCenter",
+      label: "Help Center",
+      icon: <HelpOutlineIcon/>,
+      onClick: handleClose
+    },
+    {
+      key: "logout",
+      label: "Logout",
+      icon: <Logout/>,
+      onClick: handleClose
+    }
+  ]
+
+  return (
+    <>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <Tooltip title="Profile">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            aria-controls={open ? 'profile' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32 }}/>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="profile"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        disableScrollLock={true}
+        slotProps={{
+          paper: {
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&::before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        {
+          profileMenu.map((menu, index) => (
+            <Box  key={ menu.key } >
+              <MenuItem onClick={ menu.onClick }>
+                <ListItemIcon>
+                  { menu.icon }
+                </ListItemIcon>
+                { menu.label }
+              </MenuItem>
+              { (menu.key === 'accountSettings' || menu.key === 'notifications') &&
+                profileMenu[index + 1]?.key === 'helpCenter' && <Divider />
+              }
+            </Box>
+          ))
+        }
+      </Menu>
+    </>
+  );
+}
+export default ProfileMenuPaper;

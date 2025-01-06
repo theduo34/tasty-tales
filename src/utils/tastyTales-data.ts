d15368692e34f70fb1400e7b9934d2183976c03d
Emmanuel <emmsom506@gmail.com>
2025-01-06 01:21:05 +0000

    Add new file for list of tasty tales services

---
+++ src/utils/tastyTales-data.ts
@@ -0,0 +1,28 @@
+interface TastyTalesDataInterface {
+  key: string,
+  name: string,
+  href: string
+}
+export const tastyTalesData: TastyTalesDataInterface[] = [
+  {
+    key: "whatToCook",
+    name: "What To Cook",
+    href: "/what-to-cook"
+  },
+  {
+    key: "recipes",
+    name: "Recipes",
+    href: "/recipes"
+  },
+  {
+    key: "ingredients",
+    name: "Ingredients",
+    href: "/ingredients"
+  },
+  {
+    key: "occasions",
+    name: "Occasions",
+    href: "/occasions"
+  }
+
+]

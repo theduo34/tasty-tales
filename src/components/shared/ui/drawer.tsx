131fba3681f95881fcf427fcd1833af742b458c8
Emmanuel <emmsom506@gmail.com>
2025-01-21 00:48:40 +0000

    Rename directory from utils to lib

--- src/components/shared/ui/drawer.tsx
+++ src/components/shared/ui/drawer.tsx
@@ -5,7 +5,7 @@ import IconButton from "@mui/material/IconButton"
 import MenuIcon from "@mui/icons-material/Menu"
 import Drawer from "@mui/material/Drawer"
 import React from "react";
-import {tastyTalesMenu} from "@/utils/tasty-tales-menu";
+import {tastyTalesMenu} from "@/lib/tasty-tales-menu";
 import Link from "next/link";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemIcon from "@mui/material/ListItemIcon";

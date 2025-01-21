590e9ab52d9c83f6aeff5055032f88cb4d81ac37
Emmanuel <emmsom506@gmail.com>
2025-01-21 00:44:57 +0000

    Created config for db connection

---
+++ src/config/db.ts
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+});
+
+//eslint-disable-next-line
+export const query = (text: string, params?: any[]) => pool.query(text, params);
+
+export const close = () => pool.end();

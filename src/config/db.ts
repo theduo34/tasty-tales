75e8fe201764f255455f7635c4fe10900ce0d5b0
Emmanuel <emmsom506@gmail.com>
2025-01-24 02:34:13 +0000

    update the db config to load env file using the dotenv

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,18 @@
 import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+import path from 'path';
 
-const pool = new Pool({
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
+const config = {
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
-  port: 5432,
-});
+  port: parseInt(process.env.POSTGRES_PORT || '5432', 10)
+};
+
+const pool = new Pool(config);
 
 //eslint-disable-next-line
 export const query = (text: string, params?: any[]) => pool.query(text, params);
